7d5ab3a47c73ab09e7ce1b9eb663daca6130b0dd
U1610134 <38579336+U1610134@users.noreply.github.com>
2018-05-14 16:10:44 +0300

    new change

--- IUTsocialnetwork/resources/views/welcome.blade.php
+++ IUTsocialnetwork/resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BEATYBLOG
+                    BEAUTY BLOG
                 </div>
 
                 <div class="links">
